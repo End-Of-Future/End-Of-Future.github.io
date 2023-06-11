438fb0b3a0d709109eca52bf4c729fb5adeecc67
endoffuture <liuyufan981@163.com>
2023-06-11 16:24:47 +0800

    Add server

---
+++ self-website/script.js
@@ -0,0 +1,8 @@
+const url="http://localhost:2047/login";
+function GetTopItems(){
+    var req=new XMLHttpRequest();
+    req.open("POST",url,false);
+    req.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    req.send("mode=t&code="+Math.random());
+    console.log(req.responseText);
+}
