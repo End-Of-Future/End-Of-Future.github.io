8dba58a10e234e1e94eb77e7b2f7337b1a7675bc
endoffuture <liuyufan981@163.com>
2023-06-16 22:31:25 +0800

    Add FB-Title

--- self-website/script.js
+++ self-website/script.js
@@ -1,4 +1,11 @@
 const url="http://localhost:2047/items";
+function GetTitle(){
+    var req=new XMLHttpRequest();
+    req.open("POST",url,false);
+    req.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    req.send("mode=title&code="+Math.random());
+    document.getElementById("tit").innerText=req.responseText;
+}
 function GetTopItems(){
     var req=new XMLHttpRequest();
     req.open("POST",url,false);
@@ -7,7 +14,7 @@ function GetTopItems(){
     const itArr = req.responseText.split(" ");
     var tmenu = document.getElementById("topm");
     for(it in itArr){
-        tmenu.responseText+='<a>'+it+'</a>';
+        tmenu.innerHTML+='<a>'+itArr[it]+'</a>';
     }
 }
 function GetLeftItems(path){
@@ -18,6 +25,6 @@ function GetLeftItems(path){
     const itArr = req.responseText.split(" ");
     var tmenu = document.getElementById("lmenu");
     for(it in itArr){
-        tmenu.responseText+='<a>'+it+'</a>';
+        tmenu.innerHTML+='<a>'+itArr[it]+'</a>';
     }
 }
