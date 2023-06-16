afb5a2167f255e2decd32b0ad1dbc9508cb700c0
endoffuture <liuyufan981@163.com>
2023-06-16 22:08:30 +0800

    Add LMenu

--- self-website/script.js
+++ self-website/script.js
@@ -1,8 +1,15 @@
-const url="http://localhost:2047/backend";
+const url="http://localhost:2047/items";
 function GetTopItems(){
     var req=new XMLHttpRequest();
     req.open("POST",url,false);
     req.setRequestHeader("Content-type","application/x-www-form-urlencoded");
     req.send("mode=t&code="+Math.random());
+    console.log(req.responseText);
+}
+function GetLeftItems(path){
+    var req=new XMLHttpRequest();
+    req.open("POST",url,false);
+    req.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    req.send("mode=l&path=" + path + "&code="+Math.random());
     console.log(req.responseText);
 }
